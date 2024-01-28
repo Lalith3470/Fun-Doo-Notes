4f8e0b14c5ed8ef61fa60f2d7fc9aff73841c351
Lalith Kiran Molleti <lalithkiran539@gmail.com>
2024-01-28 21:26:59 +0530

    [Lalith] Added: Project Files

---
+++ src/validators/user.validator.js
@@ -0,0 +1,13 @@
+import Joi from '@hapi/joi';
+
+export const newUserValidator = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().min(4).required()
+  });
+  const { error, value } = schema.validate(req.body);
+  if (error) {
+    next(error);
+  } else {
+    next();
+  }
+};
